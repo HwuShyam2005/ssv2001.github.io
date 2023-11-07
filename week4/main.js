ebda6afa82f2add7d44c124725318c5fac2ba009
HwuShyam2005 <shyam164825@gmail.com>
2023-11-07 11:47:17 +0400

    WEATHER

--- week4/main.js
+++ week4/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://hwushyam2005.github.io/ssv2001.github.io/cities1.json');
+ourRequest.open('GET', 'https://hwushyam2005.github.io/ssv2001.github.io/week4/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 console.log(ourData[0]);
