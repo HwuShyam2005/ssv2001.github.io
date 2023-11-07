fa1620b28fe61a42794b39e9a6026389c22a65ca
HwuShyam2005 <shyam164825@gmail.com>
2023-11-07 14:22:30 +0400

    Weather

--- week4/main.js
+++ week4/main.js
@@ -14,26 +14,11 @@ ourRequest.send();
 
 function renderHTML(data){
     for (i=0; i<data.length; i++){
-        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + "</br>" 
-        "Where you can enjoy indoor places like: " ;
-        for (ii = 0; ii < data[i].places.indoor.length; ii++) {
-        // Loop through the indoor places of the current city.
-        if (ii == 0) {
-        htmlString += data[i].places.indoor[ii];
-        } else {
-        htmlString += ", and " + data[i].places.indoor[ii];
-        }
-        }
-        htmlString += '. & enjoy outdoor places like: ';
-        // Loop through the outdoor places of the current city.
-        for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
-        if (ii == 0) {
-        htmlString += data[i].places.outdoor[ii];
-        } else {
-        htmlString += " and " + data[i].places.outdoor[ii];
-        }
-        }
-        htmlString += '.</p>';
+        htmlString += " The Weather of " + data[i].name + 
+        " is a " + data[i].type_of_sky + 
+        " the temperature is " + data[i].temperature +
+        "and the wind speed is" + data[i].windspeed +
+        "</br>"  ;
         }
     cityContainer.insertAdjacentHTML('beforeend' , htmlString);
 }
